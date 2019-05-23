a139001d7e90600abfee0cb81e5d8262a82d22d4
anoint <dbwlsk870@hanmail.net>
2019-05-23 19:50:00 +0900

    0523

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -3,26 +3,128 @@
     <div class="ui special cards">
       <div class="card">
         <div class="blurring dimmable image">
-          <div class="ui dimmer">
-            <div class="content">
-              <div class="center">
-                <div class="ui inverted button">Add Friend</div>
-              </div>
+          <div class="ui grid">
+            <div class="four wide column">
+              <img class="ui image left middle aligned" src="../img/2-01.png">
+            </div>
+            <div class="twelve wide column">
+              <h2>한국자산관리공사</h2>
+              <h3>고객의 요청을 접수하고 프로세스화로 관리하는 시스템입니다.</h3>
             </div>
           </div>
-          <img class="ui image centered middle aligned" src="../img/2-01.png">
-        </div>
-        <div class="content">
-          <a class="header">기술 스펙: html, css, js, jquery, oracle</a>
-          <div class="meta">
-            <span class="date">기간: 2019.02 ~ 2019.05</span>
+          <div class="content">
+            <a class="header">기술스펙&nbsp;&nbsp;&nbsp; JavaScript,&nbsp; jQuery,&nbsp; Oracle</a>
+            <div class="meta">
+              <span
+                class="date"
+              >기간&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 2019.02 ~ 2019.05</span>
+            </div>
           </div>
         </div>
-        <div class="extra content">
-          <a>
-            <i class="users icon"></i>
-            고객의 요청을 접수하고 프로세스화로 관리하는 시스템입니다.
-          </a>
+      </div>
+      <div class="card">
+        <div class="blurring dimmable image">
+          <div class="ui grid">
+            <div class="four wide column">
+              <img class="ui image left middle aligned" src="../img/2-02.png">
+            </div>
+            <div class="twelve wide column">
+              <h2>CFDA 접수시스템</h2>
+              <h3>중국 식품 약품 규제를 하기 위한 접수시스템입니다.</h3>
+            </div>
+          </div>
+          <div class="content">
+            <a class="header">기술스펙&nbsp;&nbsp;&nbsp; JavaScript,&nbsp; jQuery,&nbsp; Oracle</a>
+            <div class="meta">
+              <span
+                class="date"
+              >기간&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 2019.01 ~ 2019.02</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="blurring dimmable image">
+          <div class="ui grid">
+            <div class="four wide column">
+              <img class="ui image left middle aligned" src="../img/2-03.png">
+            </div>
+            <div class="twelve wide column">
+              <h2>SCOST</h2>
+              <h3>원가 예측 관리 시스템입니다.</h3>
+            </div>
+          </div>
+          <div class="content">
+            <a class="header">기술스펙&nbsp;&nbsp;&nbsp; JavaScript,&nbsp; jQuery,&nbsp; Oracle</a>
+            <div class="meta">
+              <span
+                class="date"
+              >기간&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 2018.02 ~ 2018.05</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="blurring dimmable image">
+          <div class="ui grid">
+            <div class="four wide column">
+              <img class="ui image left middle aligned" src="../img/logo.gif">
+            </div>
+            <div class="twelve wide column">
+              <h2>위즈웨어</h2>
+              <h3>UI 툴의 그룹 컴포넌트를 개발했습니다</h3>
+            </div>
+          </div>
+          <div class="content">
+            <a class="header">기술스펙&nbsp;&nbsp;&nbsp; JavaScript,&nbsp; jQuery,&nbsp; Oracle</a>
+            <div class="meta">
+              <span
+                class="date"
+              >기간&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 2019.02 ~ 2019.05</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="blurring dimmable image">
+          <div class="ui grid">
+            <div class="four wide column">
+              <img class="ui image left middle aligned" src="../img/2-04.png">
+            </div>
+            <div class="twelve wide column">
+              <h2>MADAMS</h2>
+              <h3>물질정보관리시스템입니다.</h3>
+            </div>
+          </div>
+          <div class="content">
+            <a class="header">기술스펙&nbsp;&nbsp;&nbsp; JavaScript,&nbsp; jQuery,&nbsp; Oracle</a>
+            <div class="meta">
+              <span
+                class="date"
+              >기간&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 2019.02 ~ 2019.05</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="blurring dimmable image">
+          <div class="ui grid">
+            <div class="four wide column">
+              <img class="ui image left middle aligned" src="../img/2-01.png">
+            </div>
+            <div class="twelve wide column">
+              <h2>한국자산관리공사</h2>
+              <h3>고객의 요청을 접수하고 프로세스화로 관리하는 시스템입니다.</h3>
+            </div>
+          </div>
+          <div class="content">
+            <a class="header">기술스펙&nbsp;&nbsp;&nbsp; JavaScript,&nbsp; jQuery,&nbsp; Oracle</a>
+            <div class="meta">
+              <span
+                class="date"
+              >기간&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 2019.02 ~ 2019.05</span>
+            </div>
+          </div>
         </div>
       </div>
       <div class="card">
@@ -76,12 +178,12 @@
       <div class="card">
         <div class="blurring dimmable image">
           <div class="ui inverted dimmer">
-           <div class="content">
+            <div class="content">
               <a class="header">기술 스펙: html, css, js, jquery, oracle</a>
               <div class="meta">
                 <span class="date">기간: 2018.02 ~ 2018.05</span>
               </div>
-            </div>  
+            </div>
           </div>
           <img class="ui image centered middle aligned" src="../img/logo.gif">
         </div>
@@ -94,7 +196,7 @@
         <div class="extra content">
           <a>
             <i class="users icon"></i>
-             UI 툴의 그룹 컴포넌트를 개발했습니다
+            UI 툴의 그룹 컴포넌트를 개발했습니다
           </a>
         </div>
       </div>
@@ -127,56 +229,105 @@
   </div>
 </template>
 <script>
-export default {
-  
-}
+export default {};
 </script>
 <style scoped>
-  .portFolio
-  {
-      margin: 0 auto 1rem;
-      width: 1254px;
-      border: 0; 
-  }
- .ui.card>.image, .ui.cards>.card>.image
-  {
-    background: #fff;
-    height: 300px; 
-  }
-  .ui.card>.image:not(.ui)>img, .ui.cards>.card>.image:not(.ui)>img
-  { 
-    height: 220px;
-    width: auto;
-    padding-top:80px;
-  }
-  .ui.cards
-  {
-    margin-top: 20px;
-  }
-  #app > div.portFolio > div > div:nth-child(5) > div.blurring.dimmable.image > img
-  {
-    padding-top: 120px;
-    height: 186px;
-    width: auto;
-    text-align: center;
-  }  
-  #app > div.portFolio > div > div:nth-child(3) > div.blurring.dimmable.image > img
-  {
-    width: 289px;
-    height: auto;
-    padding-left: 30px;
-    padding-right: 30px;
-    padding-top: 120px;
-  }
-#app > div.portFolio > div > div:nth-child(4) > div.blurring.dimmable.image > img
-  {
-    width: 302px;
-    height: auto;
-    padding-left: 30px;
-    padding-right: 30px;
-    padding-top: 114px;
-  }
-
+.ui.cards > .card {
+  padding-top: 10px;
+}
+.content {
+  padding-left: 30px;
+}
+a {
+  color: #59c5ff;
+}
+h2 {
+  margin-top: 30px;
+  color: #59c5ff;
+}
+.portFolio {
+  margin: 0 auto 1rem;
+  width: 1254px;
+  border: 0;
+}
+.ui.image {
+  padding: 20px 30px;
+}
+.ui.cards > .card {
+  width: 100%;
+}
+.ui.cards > .card > .image > img {
+  display: inline-block;
+}
+.ui.card > .image,
+.ui.cards > .card > .image {
+  background: #fff;
+  height: 300px;
+}
+.ui.card > .image:not(.ui) > img,
+.ui.cards > .card > .image:not(.ui) > img {
+  height: 220px;
+  width: auto;
+  /* padding-top: 80px; */
+}
+.ui.cards {
+  margin-top: 20px;
+}
+#app
+  > div.portFolio
+  > div
+  > div:nth-child(2)
+  > div
+  > div.ui.grid
+  > div.four.wide.column
+  > img {
+  padding-top: 30px;
+  padding-bottom: 50px;
+}
+#app
+  > div.portFolio
+  > div
+  > div:nth-child(5)
+  > div
+  > div.ui.grid
+  > div.four.wide.column
+  > img {
+  padding-top: 50px;
+  padding-bottom: 50px;
+  height: 150px;
+  width: auto;
+  text-align: center;
+}
+#app
+  > div.portFolio
+  > div
+  > div:nth-child(3)
+  > div
+  > div.ui.grid
+  > div.four.wide.column
+  > img {
+  width: 289px;
+  height: auto;
+  padding-left: 30px;
+  padding-right: 30px;
+  padding-top: 70px;
+  padding-bottom: 90px;
+}
+#app
+  > div.portFolio
+  > div
+  > div:nth-child(4)
+  > div
+  > div.ui.grid
+  > div.four.wide.column
+  > img {
+  width: 302px;
+  height: auto;
+  padding-left: 30px;
+  padding-right: 30px;
+  padding-top: 80px;
+  padding-bottom: 80px;
+}
 </style>
 
 
